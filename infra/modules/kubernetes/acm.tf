6c0d21a29dcf6ff1b4bb1c8bd3adb88bb82d680e
Mika Sonnleitner <63719499+msonnleitner@users.noreply.github.com>
2023-04-18 17:14:10 +0200

    enable policy controller

--- infra/modules/kubernetes/acm.tf
+++ infra/modules/kubernetes/acm.tf
@@ -16,10 +16,11 @@ resource "google_gke_hub_feature_membership" "acm" {
         secret_type = "none"
       }
     }
-  }
-  policy_controller {
+    policy_controller {
       enabled                    = true
       template_library_installed = true
+    }
   }
+
   provider = google-beta
 }
